12ca7662d7bfe7d38261fee719aebfbde3e032e8
KLEBER AUGUSTO COSTA MORENO <kleber.acmoreno@senacsp.edu.br>
2022-08-02 21:55:23 -0300

    intereacaonova

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://protected-tor-93414.herokuapp.com',
     headers: {
         'Content-Type': 'application/json'
     },
