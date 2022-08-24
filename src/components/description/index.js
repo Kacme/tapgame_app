036fed2b37c222b9219694b5cf70a6ab8aa5dcb0
KLEBER AUGUSTO COSTA MORENO <kleber.acmoreno@senacsp.edu.br>
2022-08-23 22:05:33 -0300

    alterado info

--- src/components/description/index.js
+++ src/components/description/index.js
@@ -9,8 +9,8 @@ function Description() {
                 </div>
                 <div class="description-content">
                     TapGame é um jogo desenvolvido
-                    pelos alunos do curso de desenvolvimento
-                    de jogos do SENAC - Presidente Prudente/SP
+                    pelos alunos do Curso - Programação
+                    Para Jogos - do SENAC Presidente Prudente/SP
                     em comemoração aos 40 anos da unidade.
                     Boa diversão!
                     <div class="description-btn">
